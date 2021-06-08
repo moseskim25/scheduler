9ae1a2f1debabaf2fdf84e494da79a679a7fd5b1
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-08 14:56:58 -0400

    Empty component and story

--- stories/index.js
+++ stories/index.js
@@ -9,9 +9,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from 'components/Appointment';
-import Header from 'components/Appointment/Header';
-import Empty from 'components/Appointment/Empty';
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 const days = [
   {
@@ -96,5 +96,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add('Header', () => <Header time='12pm'/>)
-  .add('Empty', () => <Empty />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
