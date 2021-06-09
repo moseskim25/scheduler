fb8d3126b8f603868b3f0b386ca67e0f38e5bea9
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-09 14:19:48 -0400

    import axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import DayList from 'components/DayList';
-
+import React, { useState, useEffect } from "react";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 import "components/Application.scss";
+import axios from 'axios';
 
 const days = [
   {
@@ -21,33 +22,46 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+];
+
+
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  
+  const [day, setDay] = useState("Monday");
+
+  const appointmentsArr = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
+  appointmentsArr.push(<Appointment key="last" time="5pm" />);
+
   return (
     <main className="layout">
       <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
+        <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={day}
-            setDay={setDay}
-          />
+          <DayList days={days} day={day} setDay={setDay} />
         </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
+        <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsArr}
       </section>
     </main>
   );
