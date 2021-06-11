8262d3733b9509e1788cdaa4a6364f3d56379cee
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-10 22:47:55 -0400

    reorganizing/commenting

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import Appointment from "components/Appointment";
 import "components/Application.scss";
 import axios from "axios";
 import { getAppointmentsForDay, getInterviewersForDay } from "helpers/selectors";
-import useVisualMode from 'hooks/useVisualMode';
+import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
@@ -13,33 +13,22 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
-
-  // const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const appointments = getAppointmentsForDay(state, state.day);
-
 
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"), 
-      axios.get("/api/appointments"),
-      axios.get('/api/interviewers')
-    ])
-      .then((all) => {
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")]).then((all) => {
       setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
-    
   }, []);
-  
-  const appointmentsArr = appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} interviewers={getInterviewersForDay(state, state.day)}/>);
+
+  //Generates array of appointments for a given day
+  const appointments = getAppointmentsForDay(state, state.day);
+  const appointmentsArr = appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} interviewers={getInterviewersForDay(state, state.day)} />);
   appointmentsArr.push(<Appointment key="last" time="5pm" />);
-
-
 
   return (
     <main className="layout">
