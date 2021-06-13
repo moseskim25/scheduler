f728434ac92a542b3b8e1a6ff24fe9d4c8549e1c
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-13 13:46:25 -0400

    restructuring

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,63 +4,18 @@ import Appointment from "components/Appointment";
 import "components/Application.scss";
 import axios from "axios";
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
+import useApplicationData from 'hooks/useApplicationData';
 
 
 export default function Application() {
   
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
-  function bookInterview(id, interview) {
-
-    // appointment object
-    // id: 1
-    // interview: {student: "name", interviewer: 9}
-    // time: "12pm"
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    //replaces that appointment id object with the new appointment
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    
-    return axios.put(`/api/appointments/${id}`, {interview})
-    .then(() => {
-      setState({...state, appointments});
-    })
-  }
-
-  function deleteInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`)
-    .then(() => {
-      setState({...state, appointments});
-    })
-  }
-
-  const setDay = (day) => setState((prev) => ({ ...prev, day }));
-
-  useEffect(() => {
-    Promise.all([axios.get("/api/days"), axios.get("/api/appointments"), axios.get("/api/interviewers")]).then((all) => {
-      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, []);
   
   //functions from helpers file
   const appointments = getAppointmentsForDay(state, state.day);
