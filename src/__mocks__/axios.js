494519e04a485be7fb64c97bcc0daa1323fd1e97
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-15 17:11:49 -0400

    added put function

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,6 +54,7 @@ const fixtures = {
 };
 
 export default {
+  defaults: { baseURL: "" },
   get: jest.fn(url => {
     if (url === "/api/days") {
       return Promise.resolve({
@@ -78,5 +79,12 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+  
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: 'No content'
+    })
   })
 }
