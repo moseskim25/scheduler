1ec0d8d01103984dd0ba313639bbd678d4dbf08f
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-06-15 12:58:42 -0400

    displays error if you try to submit an empty name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,8 +4,9 @@ import InterviewerList from "components/InterviewerList";
 import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Form(props) {
-  const [name, setName] = useState(props.student || "");
+  const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setName("");
@@ -19,6 +20,16 @@ export default function Form(props) {
     return;
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -30,8 +41,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={event => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer || (props.interview && props.interviewer)} onChange={setInterviewer}/>
       </section>
       <section className="appointment__card-right">
@@ -39,7 +52,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
